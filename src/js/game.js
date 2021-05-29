96d03678ae7f4ca694881cd1342c7406e9b8b778
Brent Conn <brent.conn@gmail.com>
2021-05-29 15:17:48 -0400

    adjust width and height for dev, removing some boilerplate code and comments

--- src/js/game.js
+++ src/js/game.js
@@ -1,36 +1,26 @@
 import * as PIXI from 'pixi.js';
 import bunny from '../images/bunny.png'
 
-console.log('This is running!');
+const gameWidth = 1280;
+const gameHeight = 720;
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
-const app = new PIXI.Application();
+const app = new PIXI.Application({
+    backgroundColor: 0xd3d3d3,
+    width: gameWidth,
+    height: gameHeight,
+});
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM
 document.body.appendChild(app.view);
 
-// load the texture we need
 app.loader.add('bunny', bunny).load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
     const bunny = new PIXI.Sprite(resources.bunny.texture);
-
-    // Setup the position of the bunny
     bunny.x = app.renderer.width / 2;
     bunny.y = app.renderer.height / 2;
-
-    // Rotate around the center
     bunny.anchor.x = 0.5;
     bunny.anchor.y = 0.5;
 
-    // Add the bunny to the scene we are building
     app.stage.addChild(bunny);
-
-    // Listen for frame updates
     app.ticker.add(() => {
-        // each frame we spin the bunny around a bit
         bunny.rotation += 0.01;
     });
 });
