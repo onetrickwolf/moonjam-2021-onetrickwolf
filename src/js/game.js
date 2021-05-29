048c4c0bdd05ea7c6934deb12e79b349b286afa5
Brent Conn <brent.conn@gmail.com>
2021-05-29 18:32:02 -0400

    adding stats

--- src/js/game.js
+++ src/js/game.js
@@ -1,12 +1,14 @@
 import * as PIXI from 'pixi.js';
 import bunny_png from '../images/bunny.png'
 
+import Stats from 'stats.js';
+
 // Twitch Integration
 
 import tmi from 'tmi.js';
 
 const client = new tmi.Client({
-    channels: ['onetrickwolf']
+    channels: ['moonmoon']
 });
 
 client.connect();
@@ -93,3 +95,26 @@ function onClick() {
         bunny.rotation += 0.01;
     });
 }
+
+// Stats
+
+var stats1 = new Stats();
+stats1.showPanel(0); // Panel 0 = fps
+stats1.domElement.style.cssText = 'position:absolute;top:0px;left:0px;';
+document.body.appendChild(stats1.domElement);
+
+var stats2 = new Stats();
+stats2.showPanel(2); // Panel 1 = ms
+stats2.domElement.style.cssText = 'position:absolute;top:0px;left:80px;';
+document.body.appendChild(stats2.domElement);
+
+function animate() {
+
+    stats1.update();
+    stats2.update();
+
+    requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
