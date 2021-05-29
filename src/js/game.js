e9f075e20f968c1b6a2000231db655d0022d05cf
Brent Conn <brent.conn@gmail.com>
2021-05-29 15:25:14 -0400

    simple text button test

--- src/js/game.js
+++ src/js/game.js
@@ -1,5 +1,5 @@
 import * as PIXI from 'pixi.js';
-import bunny from '../images/bunny.png'
+import bunny_png from '../images/bunny.png'
 
 const gameWidth = 1280;
 const gameHeight = 720;
@@ -12,15 +12,27 @@ const app = new PIXI.Application({
 
 document.body.appendChild(app.view);
 
-app.loader.add('bunny', bunny).load((loader, resources) => {
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
+let bunny;
+
+app.loader.add('bunny', bunny_png).load((loader, resources) => {
+    bunny = new PIXI.Sprite(resources.bunny.texture);
     bunny.x = app.renderer.width / 2;
     bunny.y = app.renderer.height / 2;
     bunny.anchor.x = 0.5;
     bunny.anchor.y = 0.5;
 
+    const basicText = new PIXI.Text('Basic text in pixi');
+    basicText.x = 50;
+    basicText.y = 100;
+    basicText.interactive = true;
+    basicText.buttonMode = true;
+    basicText.on('pointerdown', onClick);
+    app.stage.addChild(basicText);
+});
+
+function onClick() {
     app.stage.addChild(bunny);
     app.ticker.add(() => {
         bunny.rotation += 0.01;
     });
-});
+}
