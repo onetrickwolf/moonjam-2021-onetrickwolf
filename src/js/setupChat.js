f5dd2b08747a46de32e1dfb5bf69c0c2a4fa5257
Brent Conn <brent.conn@gmail.com>
2021-05-29 22:21:04 -0400

    more efficient distance

--- src/js/setupChat.js
+++ src/js/setupChat.js
@@ -71,7 +71,7 @@ function distanceBetweenTwoPoints(p1, p2) {
     const a = p1.x - p2.x;
     const b = p1.y - p2.y;
 
-    return Math.hypot(a, b);
+    return Math.sqrt( a*a + b*b );
 }
 
 function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
--- src/js/setupPlayer.js
+++ src/js/setupPlayer.js
@@ -73,7 +73,7 @@ function distanceBetweenTwoPoints(p1, p2) {
     const a = p1.x - p2.x;
     const b = p1.y - p2.y;
 
-    return Math.hypot(a, b);
+    return Math.sqrt( a*a + b*b );
 }
 
 export { player };
